5a7824f337be0d0b6366bfd7c537937016da05d1
sumesh kuchya <30721631+developer-sumesh@users.noreply.github.com>
2021-05-16 16:21:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({extended:true}));
 const sessionConfig ={
     secret:'bettersecret',
     unsave: true,
-    resave: false,
+    resave: true,
     saveUninitialized: true,
 }
 
